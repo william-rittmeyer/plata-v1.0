9acf0c3196cb320d7f53c2df36093db9e3c31ff0
witt-designs <wittspace77@gmail.com>
2020-04-06 17:48:09 -0400

    adding frontend files to Github pages

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -13,16 +16,16 @@ module.exports = {
                port: 8545,
                network_id: 4,
                gas: 4700000
-          }
-     },
-     compilers: {
-          solc: {
-               settings: {
-                    optimizer: {
-                         enabled: true,
-                         runs: 200
-                    }
-               }
+          },
+          ropsten: {
+               provider: () => {
+                    return new HDWalletProvider(
+                         process.env.MNEMONIC,
+                         "https://ropsten.infura.io/v3/" + process.env.INFURA_PROJECT_ID);
+          },
+               network_id: 3,
+               gas: 4500000,
+               gasPrice: 10000000000
           }
      }
 };
