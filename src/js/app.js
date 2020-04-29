9aa054664a27dcd2a82e1f51224a64fa2aa4253d
William Rittmeyer <60793567+witt-designs@users.noreply.github.com>
2020-04-29 13:08:18 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -177,13 +177,14 @@ App = {
                 if (number == _articleId){
                     console.log('https://ipfs.infura.io/ipfs/' + event.returnValues._hashvalue);
                     console.log(event.returnValues._name);
+                    hashtag = event.returnValues._hashvalue;
                     console.log(event.returnValues._seller);
                 } else {
                     return
                 }});
 
 
-                
+                console.log(hashtag);
             });
                 
                 
@@ -193,17 +194,15 @@ App = {
                console.log(_articleId);
                console.log(event.returnValues);
                
-
                
-
                 });
-
             /*    console.log('https://ipfs.infura.io/ipfs/' + event.returnValues._hashvalue);
                console.log(event.returnValues._name);
                console.log(event.returnValues._seller); */
                 
             
             console.log("transaction receipt", transactionReceipt);
+            console.log(hashtag);
 
             
 
